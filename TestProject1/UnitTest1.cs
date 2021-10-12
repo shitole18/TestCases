888df5731b757a1afe9b1ebf75016ec9037f9b5b
shitole18 <shitolerahul18@gmail.com>
2021-10-12 12:45:30 +0530

    updated using

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,12 @@
 using NUnit.Framework;
 using System;
+using System.Collections;
 
 namespace TestProject1
 {
     public class Tests
     {
-        string _name = "rahul";
+        string name = "rahul";
         [SetUp]
         public void Setup()
         {
@@ -15,9 +16,19 @@ namespace TestProject1
         [Test]
         public void Test1()
         {
+
             string name = Environment.GetEnvironmentVariable("MY_NAME", EnvironmentVariableTarget.Process);
-            Assert.AreEqual(name, "rahul");
-             
+            IDictionary envvars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
+            foreach (dynamic item in envvars)
+            {
+                if (item.Key.ToString == "MY_NAME" || item.Key == "MYNAME" || item.Key == "MYNAME")
+                {
+                    Console.WriteLine(item.Key.ToString);
+                    name = item.Key.ToString;
+                }
+            }
+            Assert.AreEqual("rahul", name);
+
         }
     }
 }
