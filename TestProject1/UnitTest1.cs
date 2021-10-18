6727f8f91d292d8800c484e6b279cddb0fe02d56
shitole18 <shitolerahul18@gmail.com>
2021-10-18 10:35:13 +0530

    envvar

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -17,16 +17,7 @@ namespace TestProject1
         public void Test1()
         {
 
-            string name = Environment.GetEnvironmentVariable("MY_NAME", EnvironmentVariableTarget.Process);
-            IDictionary envvars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
-            foreach (dynamic item in envvars)
-            {
-                if (item.Key.ToLower().ToString() == "MY_NAME" || item.Key.ToLower().ToString() == "MYNAME")
-                {
-                    Console.WriteLine(item.Key.ToString());
-                    name = item.Key.ToString;
-                }
-            }
+            string name = Environment.GetEnvironmentVariable("FIRST_NAME", EnvironmentVariableTarget.Process); 
             Assert.AreEqual("rahul", name.ToString());
 
         }
@@ -34,11 +25,10 @@ namespace TestProject1
         public void Test3()
         {
 
-            string name = Environment.GetEnvironmentVariable("MY_NAME", EnvironmentVariableTarget.Process);
             IDictionary envvars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
             foreach (dynamic item in envvars)
             {
-                if (item.Key.ToLower().ToString() == "MY_NAME" || item.Key.ToLower().ToString() == "MYNAME")
+                if (item.Key.ToLower().ToString() == "middle_name" || item.Key.ToLower().ToString() == "MYNAME")
                 {
                     Console.WriteLine(item.Key.ToString());
                     name = item.Key.ToString;
@@ -51,7 +41,7 @@ namespace TestProject1
         public void Test2()
         {
 
-            string name = Environment.GetEnvironmentVariable("MY_NAME", EnvironmentVariableTarget.Process);
+            string name = Environment.GetEnvironmentVariable("FIRST_NAME", EnvironmentVariableTarget.Process);
             IDictionary envvars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
             foreach (dynamic item in envvars)
             {
